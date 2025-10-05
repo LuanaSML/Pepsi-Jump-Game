853d843b5f9e92fd55acd9b525e8d0dc560e70d8
Luana Moura <luana.mourawork1@gmail.com>
2025-10-05 11:42:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+const sonic = document.querySelector('.sonic')
+const rock = document.querySelector('.rock')
+
+const jump = () => {
+    sonic.classList.add('jump');
+
+    setTimeout( () => {
+        sonic.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    console.log ('loop');
+    const rockposition = rock.offsetLeft;
+    const sonicposition = + window.getComputedStyle(sonic).bottom.replace('px', '');
+
+    console.log(sonicposition);
+
+    if (rockposition <= 110 && rockposition > 0 && sonicposition < 125){ {
+        rock.style.animation = 'none' ;
+        rock.style.left = `${rockposition}px`;
+
+        sonic.style.animation = 'none' ;
+        sonic.style.bottom = `${sonicposition}px`;
+        sonic.src = './IMG/MORRENDO.gif';
+        sonic.style.width = '200px';
+    
+        document.querySelector('.ground').style.animation = 'none';
+        clearInterval(loop);
+        
+        setTimeout(() => {
+        window.location.href = 'gameover.html';
+    }, 1300); 
+
+    }
+    }
+
+
+    
+}, 10);
+
+
+
+document.addEventListener('keydown', jump)
+
